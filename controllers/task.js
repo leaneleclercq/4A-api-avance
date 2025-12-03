b924e4269654da82ce93a0025b576174c28a0cc3
leaneleclercq <l_leclercq4@etu-webschoolfactory.fr>
2025-12-03 17:28:06 +0100

    mise à jour

--- controllers/task.js
+++ controllers/task.js
@@ -7,7 +7,7 @@ module.exports = {
     post: async (req, res, next) => {
         const newData = req.body;
         const newtask = await TaskModel.create(newData); // crée une nouvelle tache avec les données reçues par newData, newTask est l'objet mis à jour 
-        res.json(newtask); // renvoie l'objet newTask en json, la valeur de l'objet créé
+        res.status(201).json(newtask); // renvoie l'objet newTask en json, la valeur de l'objet créé, 201 signifie que la ressource a été créée
     },
     get: (req, res, next) => {},
     patch: (req, res, next) => {},
