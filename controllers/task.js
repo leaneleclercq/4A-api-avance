988dafb4b8d38a0db41583d2efe4513f5ebd380b
leaneleclercq <l_leclercq4@etu-webschoolfactory.fr>
2025-12-03 17:14:14 +0100

    deuxième commit

--- controllers/task.js
+++ controllers/task.js
@@ -1,9 +1,12 @@
 const TaskModel = require('../models/task.js');
 
 module.exports = {
-    cget: (req, res, next) => {},
+    cget: async (req, res, next) => {
+        res.json(await TaskModel.findAll());
+    },
     post: (req, res, next) => {},
     get: (req, res, next) => {},
     patch: (req, res, next) => {},
     delete: (req, res, next) => {}
 };
+
